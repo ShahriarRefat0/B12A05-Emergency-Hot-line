dc53e58bb04d25c2c9045342e636e194d9069ea9
Shahriar Refat <shahariarrefat@gmail.com>
2025-08-27 16:22:19 +0600

    copy text and counting Functionalities add

--- js/index.js
+++ js/index.js
@@ -7,69 +7,20 @@ document
       document.getElementById("heart-number").innerText = newHeatNumber;
     }
 
-    // if (e.target.className.includes("copy-btn")) {
-    //   const copyBtn = e.target;
-    //   const copyNumber = copyBtn.parentNode.parentNode.children[2].innerText;
-
-    //   console.log(copyNumber)
-    //    navigator.clipboard.writeText(copyNumber).then(() => {
-    //      //  alert("Text copied: " + callNumber);
-    //      console.log(copyNumber);
-    //    });
-    // };
-
-    // Check if the clicked element (or inside) is a button with copy-btn
-    if (e.target.classList.contains("copy-btn")) {
+    if (e.target.className.includes("copy-btn")) {
       const copyBtn = e.target;
+      const copyNumber = copyBtn.parentNode.parentNode.children[2].innerText;
 
-      // go up to the card
-      const card = copyBtn.parentNode.parentNode;
+      
+        navigator.clipboard.writeText(copyNumber).then(() => {
+          alert("Text copied: " + copyNumber);
+    });
+      
+        const copyCount = document.getElementById("copy-count").innerText;
+         const currentCopyCount = Number(copyCount) + 1;
+         document.getElementById("copy-count").innerText = currentCopyCount;
+      
+    };
 
-      // find the specific text (example: 3rd child)
-      const copyNumber = card.children[2].innerText;
 
-      console.log("Text to copy:", copyNumber);
-
-      navigator.clipboard.writeText(copyNumber).then(() => {
-        console.log("Copied:", copyNumber);
-        alert("Copied: " + copyNumber);
-      });
-    }
-
-    if (e.target.className.includes("call-btn")) {
-      const callBtn = e.target;
-      const serviceNameEng =
-        callBtn.parentNode.parentNode.children[1].children[1].innerText;
-      const callNumber = callBtn.parentNode.parentNode.children[2].innerText;
-      const coins = document.getElementById("conis").innerText;
-      if (coins <= 0) {
-        alert(`❌ আপনার পর্যাপ্ত কয়েন নেই। কল করতে কমপক্ষে ২০ কয়েন লাগবে।`);
-        return;
-      } else {
-        alert(`📞 Calling ${serviceNameEng} ${callNumber}...`);
-      }
-
-      const currentCoin = Number(coins) - 20;
-      document.getElementById("conis").innerText = currentCoin;
-
-      const serviceNameBng =
-        callBtn.parentNode.parentNode.children[1].children[0].innerText;
-
-      const historyContainer = document.getElementById("history-container");
-
-      const now = new Date();
-      const localTimeString = now.toLocaleTimeString();
-
-      const newHistory = document.createElement("div");
-      newHistory.innerHTML = `
-    <div class="flex p-5 justify-between text-[20px] mt-3 items-center bg-gray-100 rounded-md">
-            <div>
-              <p>${serviceNameBng}</p>
-              <p>${callNumber}</p>
-            </div>
-            <p>${localTimeString}</p>
-          </div>
-    `;
-      historyContainer.appendChild(newHistory);
-    }
-  });
+   
